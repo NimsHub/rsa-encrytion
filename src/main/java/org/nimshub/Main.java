4b3c33872389d467dd47634bf1104726b00da54c
nirmala <nirmalasabaragamuwa@gmail.com>
2023-08-17 20:47:54 +0530

    refactor

--- src/main/java/org/nimshub/Main.java
+++ src/main/java/org/nimshub/Main.java
@@ -1,7 +1,24 @@
 package org.nimshub;
 
+import org.nimshub.cipher.RSA;
+import org.nimshub.utils.Converters;
+import org.nimshub.utils.Printer;
+
+import static org.nimshub.utils.Constants.STUDENT_NAME;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        RSA rsa = new RSA();
+        String teststring = STUDENT_NAME;
+
+        Printer.print("String in Bytes: "
+                + Converters.bytesToString(teststring.getBytes()));
+        // encrypt
+        byte[] encrypted = rsa.encrypt(teststring.getBytes());
+        // decrypt
+        byte[] decrypted = rsa.decrypt(encrypted);
+        Printer.print("Decrypting Bytes: " + Converters.bytesToString(decrypted));
+        Printer.print("Decrypted String: " + new String(decrypted));
     }
 }
--- src/main/java/org/nimshub/cipher/RSA.java
+++ src/main/java/org/nimshub/cipher/RSA.java
@@ -1,64 +1,65 @@
 package org.nimshub.cipher;
 
-import org.nimshub.utils.Converters;
+import org.nimshub.utils.Logger;
 
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
 import static org.nimshub.utils.Constants.BIT_LENGTH;
 
+/**
+ * Implementation of RSA Encryption
+ *
+ * @author Sabaragamuwa S.B.N.M
+ * @regNo EG/2018/3443
+ */
 public class RSA {
+    Logger logger = new Logger();
+    private final BigInteger modulus;
+    private final BigInteger publicKey;
+    private final BigInteger privateKey;
 
-    private final BigInteger N;
-    private final BigInteger e;
-        private final BigInteger d;
+    public RSA() {
+        SecureRandom random = new SecureRandom();
 
-    public RSA()
-        {
-            SecureRandom random = new SecureRandom();;
-            BigInteger p = BigInteger.probablePrime(BIT_LENGTH, random);
-            BigInteger q = BigInteger.probablePrime(BIT_LENGTH, random);
-            N = p.multiply(q);
-            BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-            e = BigInteger.probablePrime(BIT_LENGTH / 2, random);
-            while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
-            {
-                e.add(BigInteger.ONE);
-            }
-            d = e.modInverse(phi);
-        }
+        logger.info("generating two large prime numbers p and q");
+        BigInteger p = BigInteger.probablePrime(BIT_LENGTH, random);
+        logger.info(String.format("randomly assigned value for p : %s %n",p));
+        BigInteger q = BigInteger.probablePrime(BIT_LENGTH, random);
+        logger.info(String.format("randomly assigned value for q : %s %n",q));
 
-        public static void main(String[] args) throws IOException
-        {
-            RSA rsa = new RSA();
-            DataInputStream in = new DataInputStream(System.in);
-            String teststring;
-            System.out.println("Enter the plain text:");
-            teststring = in.readLine();
-            System.out.println("Encrypting String: " + teststring);
-            System.out.println("String in Bytes: "
-                    + Converters.bytesToString(teststring.getBytes()));
-            // encrypt
-            byte[] encrypted = rsa.encrypt(teststring.getBytes());
-            // decrypt
-            byte[] decrypted = rsa.decrypt(encrypted);
-            System.out.println("Decrypting Bytes: " + Converters.bytesToString(decrypted));
-            System.out.println("Decrypted String: " + new String(decrypted));
-        }
+        modulus = p.multiply(q);
+        logger.info(String.format("calculated value of modulus from p and q : %s %n",modulus));
+
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        logger.info(String.format("calculated totient function : %s %n",phi));
+
+        publicKey = BigInteger.probablePrime(BIT_LENGTH / 2, random);
+
+        privateKey = publicKey.modInverse(phi);
+    }
 
 
+    /**
+     * Encrypt a given message
+     *
+     * @param message : byte[]
+     * @return byte[]
+     */
+    public byte[] encrypt(byte[] message) {
+        logger.info("encrypting message ...");
+        return (new BigInteger(message)).modPow(publicKey, modulus).toByteArray();
 
-        // Encrypt message
-        public byte[] encrypt(byte[] message)
-        {
-            return (new BigInteger(message)).modPow(e, N).toByteArray();
-        }
+    }
 
-        // Decrypt message
-        public byte[] decrypt(byte[] message)
-        {
-            return (new BigInteger(message)).modPow(d, N).toByteArray();
-        }
+    /**
+     * This method decrypt encrypted message
+     *
+     * @param message : byte[]
+     * @return byte[]
+     */
+    public byte[] decrypt(byte[] message) {
+        logger.info("decrypting message ...");
+        return (new BigInteger(message)).modPow(privateKey, modulus).toByteArray();
+    }
 }
